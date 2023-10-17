4188cc04ca940bc78c47523be39e831918c0f152
MateoChacon <xdmateo90@gmail.com>
2023-10-17 14:48:20 -0500

    22. directiva: v-for

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,18 @@
 <script setup lang="ts">
-import { ref } from 'vue';
 
 const name = "Vue dinamico";
-const active = null;
 
-let num = ref('');
-
-const colorArray = ['blue', 'green', 'black'];
-let count = 0;
-const colorChange = () => {
-  count = (count + 1) % colorArray.length;
-  num.value = colorArray[count];
-}
-
-
-colorChange()
+const fruitArray = ["🍉", "🍊", "🍋", "🍌", "🍍", "🥭"]
 </script>
 
 <template>
   <h1>
     Hola {{ name.toUpperCase() }}
   </h1>
-  <p>{{ active ? "I'm active" : "Don't active" }}</p>
-  <p v-if="active">Active</p>
-  <p v-else-if="active === false">Don't active</p>
-  <p v-else>Estoy indeciso</p>
-  <h2 @click="colorChange()" :style="`color: ${num}`">Soy {{ num }}</h2>
-  <h2 v-show="active">active</h2>
+  <p>{{ fruitArray }}</p>
+  <ul>
+    <li v-for="(fruit, index) in fruitArray" :key="index">{{ index }} - {{ fruit }}</li>
+  </ul>
 </template>
 
 <style>
