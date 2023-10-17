9e6310e7e6d76507e8d6a53d7ecbeaa69a04b70f
MateoChacon <xdmateo90@gmail.com>
2023-10-17 12:28:39 -0500

    20. Uso de expreciones de js

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,28 @@
-<script setup>
-const name = "Vue dinamico";
-const colorBlue = "color: blue";
+<script setup lang="ts">
+import { ref } from 'vue';
 
+const name = "Vue dinamico";
+const active = true;
+
+let num = ref('');
+
+const colorArray = ['blue', 'green', 'black'];
+let count = 0;
+const colorChange = () => {
+  count = (count + 1) % colorArray.length;
+  num.value = colorArray[count];
+}
+
+
+colorChange()
 </script>
 
 <template>
   <h1>
-    Hola {{ name }}
-    <h2 :style="colorBlue">Soy azul</h2>
+    Hola {{ name.toUpperCase() }}
   </h1>
+  <p>{{ active ? "I'm active" : "Don't active" }}</p>
+  <h2 @click="colorChange()" :style="`color: ${num}`">Soy {{ num }}</h2>
 </template>
 
 <style>
