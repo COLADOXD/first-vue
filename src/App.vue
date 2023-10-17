3b915dd11e99ad693bf268363eb0ec6d2ddfc172
MateoChacon <xdmateo90@gmail.com>
2023-10-17 15:15:14 -0500

    23. v-for array de objetos

--- src/App.vue
+++ src/App.vue
@@ -2,7 +2,26 @@
 
 const name = "Vue dinamico";
 
-const fruitArray = ["🍉", "🍊", "🍋", "🍌", "🍍", "🥭"]
+const fruitArray = [
+  {
+    name: "Manzana",
+    price: "$1.00",
+    description: "Una manzana",
+    stock: 0,
+  },
+  {
+    name: "Pera",
+    price: "$2.00",
+    description: "Una pera",
+    stock: 10,
+  },
+  {
+    name: "Naranja",
+    price: "$3.00",
+    description: "Una naranja",
+    stock: 20,
+  },
+];
 </script>
 
 <template>
@@ -11,7 +30,9 @@ const fruitArray = ["🍉", "🍊", "🍋", "🍌", "🍍", "🥭"]
   </h1>
   <p>{{ fruitArray }}</p>
   <ul>
-    <li v-for="(fruit, index) in fruitArray" :key="index">{{ index }} - {{ fruit }}</li>
+    <li v-for="(fruit, index) in fruitArray" :key="fruit.name">{{ fruit.stock }} - {{ fruit.name }} - {{ fruit.price }} :
+      {{ fruit.description }}
+    </li>
   </ul>
 </template>
 
