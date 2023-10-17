248a4aecf649c54adabf0d2b9adedab777805ba4
MateoChacon <xdmateo90@gmail.com>
2023-10-17 15:23:44 -0500

    24. recorriendo objetos con v-for

--- src/App.vue
+++ src/App.vue
@@ -2,36 +2,23 @@
 
 const name = "Vue dinamico";
 
-const fruitArray = [
-  {
-    name: "Manzana",
-    price: "$1.00",
-    description: "Una manzana",
-    stock: 0,
-  },
-  {
-    name: "Pera",
-    price: "$2.00",
-    description: "Una pera",
-    stock: 10,
-  },
-  {
-    name: "Naranja",
-    price: "$3.00",
-    description: "Una naranja",
-    stock: 20,
-  },
-];
+//Cuando se recorre un objeto primero es el valor, propiedad y al ultimo el index
+const fruitObj = {
+  name: "Manzana",
+  price: "$1.00",
+  description: "Una manzana",
+  stock: 0,
+}
 </script>
 
 <template>
   <h1>
     Hola {{ name.toUpperCase() }}
   </h1>
-  <p>{{ fruitArray }}</p>
+  <p>{{ fruitObj }}</p>
   <ul>
-    <li v-for="(fruit, index) in fruitArray" :key="fruit.name">{{ fruit.stock }} - {{ fruit.name }} - {{ fruit.price }} :
-      {{ fruit.description }}
+    <li v-for="(value, property, index) in fruitObj" :key="value">
+      {{ `${index} - ${property}: ${value}` }}
     </li>
   </ul>
 </template>
