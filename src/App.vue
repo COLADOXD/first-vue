8351e894895262805387352d778c114a408902fe
MateoChacon <xdmateo90@gmail.com>
2023-10-18 12:37:18 -0500

    28. Evento Click (v-on)

--- src/App.vue
+++ src/App.vue
@@ -2,40 +2,16 @@
 
 const name = "Vue dinamico";
 
-const arrayFrutas = [
-  {
-    name: "Manzana",
-    price: "$1.00",
-    description: "Una manzana",
-    stock: 0,
-  },
-  {
-    name: "Pera",
-    price: "$2.00",
-    description: "Una pera",
-    stock: 10,
-  },
-  {
-    name: "Naranja",
-    price: "$3.00",
-    description: "Una naranja",
-    stock: 20,
-  },
-];
+const handleClick = () => {
+  console.log("Click");
+}
 </script>
 
 <template>
   <h1>
     Hola {{ name.toUpperCase() }}
   </h1>
-  <p>{{ arrayFrutas }}</p>
-  <ul>
-    <template v-for="(item, index) in arrayFrutas" :key="item.name">
-      <li v-if="item.stock > 0">
-        {{ item.name }} {{ item.price }}
-      </li>
-    </template>
-  </ul>
+  <button @click="handleClick()">active</button>
 </template>
 
 <style>
