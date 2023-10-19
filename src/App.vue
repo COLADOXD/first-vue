3c9d0edd29fb9f5ca1193c9e46b8cc446ef04ee0
MateoChacon <xdmateo90@gmail.com>
2023-10-19 15:42:08 -0500

    34. Solución Desafío

--- src/App.vue
+++ src/App.vue
@@ -4,23 +4,15 @@ import { ref } from 'vue';
 
 const name = "Vue dinamico";
 
-let counter = ref(0)
-
-const increment = () => {
-  counter.value += 1;
-}
+let counter = ref(0);
 </script>
 
 <template>
   <h1>
     Hola {{ name.toUpperCase() }}
   </h1>
-  <h2>{{ counter }}</h2>
-  <button @click="increment">active</button>
+  <h2 :style="counter >= 0 ? 'color: green' : 'color: red'">{{ counter }}</h2>
+  <button @click="counter++">increment</button>
+  <button @click="counter--">reduce</button>
+  <button @click="counter = 0">reset</button>
 </template>
-
-<style>
-h1 {
-  color: red;
-}
-</style>
