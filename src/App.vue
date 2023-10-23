2a4d96504c2783e0304bcbba244461e4fb9fd29e
MateoChacon <xdmateo90@gmail.com>
2023-10-23 15:37:17 -0500

    35. Propiedades computadas o calculadas

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,29 @@
 <script setup lang="ts">
-import { ref } from 'vue';
+import { ref, computed } from 'vue';
 
 
 const name = "Vue dinamico";
 
 let counter = ref(0);
+
+const classComputer = computed(() => {
+  if (counter.value === 0) {
+    return 'zero'
+  }
+  if (counter.value > 0) {
+    return 'positive'
+  }
+  if (counter.value) {
+    return 'negative'
+  }
+});
 </script>
 
 <template>
   <h1>
     Hola {{ name.toUpperCase() }}
   </h1>
-  <h2 :style="counter >= 0 ? 'color: green' : 'color: red'">{{ counter }}</h2>
+  <h2 :style="counter >= 0 ? 'color: green' : 'color: red'">{{ classComputer }}</h2>
   <h2 :style="counter >= 0 ? 'color: green' : 'color: red'">{{ counter }}</h2>
   <button @click="counter++">increment</button>
   <button @click="counter--">reduce</button>
