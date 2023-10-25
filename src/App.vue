22d4d6f0dc988287f62b1600d4c3ac16cb085800
MateoChacon <xdmateo90@gmail.com>
2023-10-25 14:47:52 -0500

    37. Solución práctica

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,11 @@
 <script setup lang="ts">
-import { ref, computed } from 'vue';
+import { ref, computed, Ref } from 'vue';
 
 
 const name = "Vue dinamico";
 
 let counter = ref(0);
+let counterArray: Ref<number[]> = ref([]);
 
 const classComputer = computed(() => {
   if (counter.value === 0) {
@@ -17,6 +18,14 @@ const classComputer = computed(() => {
     return 'negative'
   }
 });
+
+const addNumber = computed(() => {
+  const disabled = counterArray.value.find((number) => number === counter.value);
+  if (disabled === 0) {
+    return true
+  }
+  return disabled ? true : false
+})
 </script>
 
 <template>
@@ -25,8 +34,13 @@ const classComputer = computed(() => {
   </h1>
   <h2 :style="counter >= 0 ? 'color: green' : 'color: red'">{{ classComputer }}</h2>
   <h2 :style="counter >= 0 ? 'color: green' : 'color: red'">{{ counter }}</h2>
+  <h2>{{ counterArray }}</h2><br>
+  <ul>
+    <li v-for="number in counterArray">
+      {{ number }}</li>
+  </ul>
   <button @click="counter++">increment</button>
   <button @click="counter--">reduce</button>
   <button @click="counter = 0">reset</button>
-  <button :disabled="false">add</button>
+  <button :disabled="addNumber" @click="counterArray.push(counter)">add</button>
 </template>
